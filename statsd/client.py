c2a5514d5954248cf53d171eec7601d6163dffb6
James Socol <james@mozilla.com>
2011-08-24 19:15:06 -0400

    Naive context decorator.

--- statsd/client.py
+++ statsd/client.py
@@ -1,37 +1,50 @@
 from contextlib import contextmanager
+from functools import wraps
 import random
 import socket
 import time
 
 
+class Timer(object):
+    """A contextdecorator for timing."""
+    def __init__(self, cl):
+        self.client = cl
+
+    def __call__(self, stat, rate=1):
+        def decorator(fn):
+            @wraps(fn)
+            def wrapped(*a, **kw):
+                with self(stat, rate=rate):
+                    return fn(*a, **kw)
+            return wrapped
+        return decorator
+
+    def __enter__(self, stat, rate=1):
+        self.start = time.time()
+        self.stat = stat
+        self.rate = rate
+
+    def __exit__(self, typ, value, tb):
+        dt = time.time() - self.start
+        dt = int(round(dt * 1000))  # Convert to ms.
+        self.client.timing(self.stat, dt, self.rate)
+        if any((typ, value, rb)):
+            raise typ, value, tb
+
+
 class StatsClient(object):
     """A client for statsd."""
+
     def __init__(self, host='localhost', port=8125, prefix=None):
         """Create a new client."""
         self._addr = (host, port)
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.prefix = prefix
+        self.timer = Timer(self)
 
     def timing(self, stat, delta, rate=1):
         """Send new timing information. `delta` is in milliseconds."""
         self._send(stat, '%d|ms' % delta, rate)
-
-    @contextmanager
-    def timer(self, stat, rate=1):
-        """A context manager to time sections of code.
-
-        >>> c = StatsClient()
-        >>> with c.timer('foo'):
-        ...     do_something()
-
-        When the block finishes, the time it took will automatically be
-        sent to statsd.
-        """
-        start = time.time()
-        yield
-        dt = time.time() - start
-        dt = int(round(dt * 1000))
-        self.timing(stat, dt, rate)
 
     def incr(self, stat, count=1, rate=1):
         """Increment a stat by `count`."""
