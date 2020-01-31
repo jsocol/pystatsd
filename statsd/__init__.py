f23330f119de4b12446f75fd4851f41b0b521edd
Nikita Shenkman <nikita.shenkman@gmail.com>
2020-01-31 17:47:55 -0500

    im an idiot

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import
 from .client import StatsClient
 from .client import TCPStatsClient
 from .client import UnixSocketStatsClient
-
+from .client import ConsistentHashingStatsClient
 
 VERSION = (3, 2, 1)
 __version__ = '.'.join(map(str, VERSION))
-__all__ = ['StatsClient', 'TCPStatsClient', 'UnixSocketStatsClient']
+__all__ = ['StatsClient', 'TCPStatsClient', 'UnixSocketStatsClient', 'ConsistentHashingStatsClient']
