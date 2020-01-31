7452f2950f7fd15ae1bcf43a5edc9535738a7e7f
Nikita Shenkman <nikita.shenkman@gmail.com>
2020-01-31 17:08:05 -0500

    keep trying

--- statsd/client/__init__.py
+++ statsd/client/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import, division, unicode_literals
 
 from .stream import TCPStatsClient, UnixSocketStatsClient  # noqa
 from .udp import StatsClient  # noqa
-from .consistent_hashing import ConsistentHashingStatsClient  # noqa
+from .consistent_hashing import ConsistentHashingStatsClient as StatsClient  # noqa
