081c4238747d3b35329f640342808a49bb26d21b
Nikita Shenkman <nikita.shenkman@gmail.com>
2020-01-31 17:51:04 -0500

    try this out

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
-from .client import StatsClient
+from .client import StatsClient as UDPStatsClient
 from .client import TCPStatsClient
 from .client import UnixSocketStatsClient
-from .client import ConsistentHashingStatsClient
+from .client import ConsistentHashingStatsClient as StatsClient
 
 VERSION = (3, 2, 1)
 __version__ = '.'.join(map(str, VERSION))
-__all__ = ['StatsClient', 'TCPStatsClient', 'UnixSocketStatsClient', 'ConsistentHashingStatsClient']
+__all__ = ['UDPStatsClient', 'TCPStatsClient', 'UnixSocketStatsClient', 'StatsClient']
