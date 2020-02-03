6c73293e60cf47981db250546dfa459ca2ac07f2
Nikita Shenkman <nikita.shenkman@gmail.com>
2020-02-03 09:17:37 -0500

    change the defaults

--- statsd/defaults/django.py
+++ statsd/defaults/django.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from django.conf import settings
 
 from statsd import defaults
-from statsd.client import StatsClient
+from statsd.client import ConsistentHashingStatsClient
 
 
 statsd = None
@@ -13,5 +13,5 @@ if statsd is None:
     prefix = getattr(settings, 'STATSD_PREFIX', defaults.PREFIX)
     maxudpsize = getattr(settings, 'STATSD_MAXUDPSIZE', defaults.MAXUDPSIZE)
     ipv6 = getattr(settings, 'STATSD_IPV6', defaults.IPV6)
-    statsd = StatsClient(host=host, port=port, prefix=prefix,
+    statsd = ConsistentHashingStatsClient(host=host, port=port, prefix=prefix,
                          maxudpsize=maxudpsize, ipv6=ipv6)
--- statsd/defaults/env.py
+++ statsd/defaults/env.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import os
 
 from statsd import defaults
-from statsd.client import StatsClient
+from statsd.client import ConsistentHashingStatsClient
 
 
 statsd = None
@@ -13,5 +13,5 @@ if statsd is None:
     prefix = os.getenv('STATSD_PREFIX', defaults.PREFIX)
     maxudpsize = int(os.getenv('STATSD_MAXUDPSIZE', defaults.MAXUDPSIZE))
     ipv6 = bool(int(os.getenv('STATSD_IPV6', defaults.IPV6)))
-    statsd = StatsClient(host=host, port=port, prefix=prefix,
+    statsd = ConsistentHashingStatsClient(host=host, port=port, prefix=prefix,
                          maxudpsize=maxudpsize, ipv6=ipv6)
