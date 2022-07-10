d93b18bad73148088eb5d09e4821a909880a27ca
Matthew Hughes <matthewhughes934@gmail.com>
2022-07-10 14:06:01 +0100

    Avoid skipping test
    I want to see if this passes in CI. If it doesn't I might just ignore it
    while in CI runs.

--- tests/statsd_test.py
+++ tests/statsd_test.py
@@ -3,7 +3,7 @@ import random
 import re
 import socket
 from datetime import timedelta
-from unittest import SkipTest, mock
+from unittest import mock
 
 import pytest
 
@@ -202,7 +202,6 @@ def _test_resolution(cl, proto, addr):
 
 
 def test_ipv6_resolution_udp():
-    raise SkipTest("IPv6 resolution is broken on Travis")
     cl = _udp_client(addr="localhost", ipv6=True)
     _test_resolution(cl, "udp", ("::1", 8125, 0, 0))
 
