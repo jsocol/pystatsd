7d68cc63961561f968508e2bea43ba811ac7a664
Matthew Hughes <matthewhughes934@gmail.com>
2022-07-15 21:31:50 +0100

    Add typing annotations for `statsd.client.timer`
    I've left some `type: ignore` comments due to missing annotations in
    `statsd.Client` but these should be cleaned up once annotations are
    added there

--- statsd/client/timer.py
+++ statsd/client/timer.py
@@ -1,15 +1,24 @@
+from __future__ import annotations
+
 import functools
+from collections.abc import Callable, Sequence
+from contextlib import AbstractContextManager
+from time import perf_counter
+from types import TracebackType
+from typing import TYPE_CHECKING, TypeVar
 
-# Use timer that's not susceptible to time of day adjustments.
-try:
-    # perf_counter is only present on Py3.3+
-    from time import perf_counter as time_now
-except ImportError:
-    # fall back to using time
-    from time import time as time_now
+if TYPE_CHECKING:
+    from .base import StatsClientBase
+
+    R = TypeVar("R")
+    C = TypeVar("C", bound=StatsClientBase)
 
 
-def safe_wraps(wrapper, *args, **kwargs):
+def safe_wraps(
+    wrapper: Callable[..., R] | functools.partial[R],
+    *args: Sequence[str],
+    **kwargs: Sequence[str],
+) -> Callable[..., Callable[..., R]]:
     """Safely wraps partial functions."""
     while isinstance(wrapper, functools.partial):
         wrapper = wrapper.func
@@ -19,53 +28,58 @@ def safe_wraps(wrapper, *args, **kwargs):
 class Timer:
     """A context manager/decorator for statsd.timing()."""
 
-    def __init__(self, client, stat, rate=1):
+    def __init__(self, client: C, stat: str, rate: float = 1):
         self.client = client
         self.stat = stat
         self.rate = rate
-        self.ms = None
+        self.ms: float | None = None
         self._sent = False
-        self._start_time = None
+        self._start_time: float | None = None
 
-    def __call__(self, f):
+    def __call__(self, f: Callable[..., R]) -> Callable[..., R]:
         """Thread-safe timing function decorator."""
 
         @safe_wraps(f)
-        def _wrapped(*args, **kwargs):
-            start_time = time_now()
+        def _wrapped(*args: object, **kwargs: object) -> R:
+            start_time = perf_counter()
             try:
                 return f(*args, **kwargs)
             finally:
-                elapsed_time_ms = 1000.0 * (time_now() - start_time)
-                self.client.timing(self.stat, elapsed_time_ms, self.rate)
+                elapsed_time_ms = 1000.0 * (perf_counter() - start_time)
+                self.client.timing(self.stat, elapsed_time_ms, self.rate)  # type: ignore[no-untyped-call]
 
         return _wrapped
 
-    def __enter__(self):
+    def __enter__(self) -> Timer:
         return self.start()
 
-    def __exit__(self, typ, value, tb):
+    def __exit__(
+        self,
+        typ: type[BaseException] | None,
+        value: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None:
         self.stop()
 
-    def start(self):
+    def start(self) -> Timer:
         self.ms = None
         self._sent = False
-        self._start_time = time_now()
+        self._start_time = perf_counter()
         return self
 
-    def stop(self, send=True):
+    def stop(self, send: bool = True) -> Timer:
         if self._start_time is None:
             raise RuntimeError("Timer has not started.")
-        dt = time_now() - self._start_time
+        dt = perf_counter() - self._start_time
         self.ms = 1000.0 * dt  # Convert to milliseconds.
         if send:
             self.send()
         return self
 
-    def send(self):
+    def send(self) -> None:
         if self.ms is None:
             raise RuntimeError("No data recorded.")
         if self._sent:
             raise RuntimeError("Already sent data.")
         self._sent = True
-        self.client.timing(self.stat, self.ms, self.rate)
+        self.client.timing(self.stat, self.ms, self.rate)  # type: ignore[no-untyped-call]
